1a8fe79d098bfe38be86fc2758bef50d6ca2b240
CindyKatoni <katmuema@gmail.com>
2020-09-28 17:22:25 +0300

    create the home views in home/views.html

---
+++ app/home/views.py
@@ -0,0 +1,19 @@
+#Views for home page
+from flask import render_template
+from flask_login import login_required
+
+from . import home
+
+@home.route('/')
+def landingpage():
+    """
+    Render the landing home page template on the / route
+    """
+    return render_template('home/index.html', title="Welcome to the home page")
+
+@home.route('/dashboard')
+def dashboardpage():
+    """
+    Render the dashboard page template on the /dashboard route
+    """
+    return render_template('home/dashboard.html', title="Welcome to the dashboard")    
