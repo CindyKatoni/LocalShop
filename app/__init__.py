580df6f0f834c82ffad48e2e92459606fed3ca91
CindyKatoni <katmuema@gmail.com>
2020-09-30 19:49:07 +0300

    post merge conflict fixes & updates

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,15 @@
 #3rd Party Imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 #local imports
 from config import app_config
 
 #Db initialization
 db = SQLAlchemy()
+
+login_manager = LoginManager()
 
 def create_app(config_name):
     '''
@@ -21,8 +23,18 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    # login_manager.login_view = "auth.login"
+
     # Register Home Blueprint
     from app import models
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
     
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
